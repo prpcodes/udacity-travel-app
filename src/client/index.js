508934c28069f740654a2431859a7284c9096834
Philipp P <office@prp.wien>
2020-07-02 14:41:18 +0200

    added geoHandler.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,10 @@
 import { formHandler } from "./js/formHandler";
 import { updateUi } from "./js/updateUi";
+import { geoHandler } from "./js/geoHandler";
 
 import "./styles/style.scss";
 
 // addEventListener
-document.getElementById("generate").addEventListener("click", formHandler);
+document.getElementById("generate").addEventListener("click", geoHandler);
 
-export { formHandler, updateUi };
+export { formHandler, updateUi, geoHandler };
