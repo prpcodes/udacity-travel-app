2ba3252d21331cf788d4295162b1d189a62fb434
Philipp P <office@prp.wien>
2020-07-02 14:43:25 +0200

    deleted formHandler.js

--- src/client/js/formHandler.js
+++
@@ -1,8 +0,0 @@
-function formHandler() {
-  console.log("click");
-  // get location data from document
-  const location = document.getElementById("location").value;
-  const departure = document.getElementById("departure").value;
-}
-
-export { formHandler };
