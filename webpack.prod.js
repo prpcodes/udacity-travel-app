fa7890e7008af9fdfcdb2ab838881d7156f664f9
Philipp P <office@prp.wien>
2020-07-08 23:40:41 +0200

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -30,10 +30,12 @@ module.exports = {
       filename: "./index.html",
       title: "Progressive Web Application",
     }),
+    /*
     new WorkboxPlugin.GenerateSW({
       clientsClaim: true,
       skipWaiting: true,
     }),
+    */
     new CleanWebpackPlugin({
       // Simulate the removal of files
       dry: true,
