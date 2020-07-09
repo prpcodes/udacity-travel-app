894bb0425b608f685decb1618584f28bbcc1bc17
Philipp P <office@prp.wien>
2020-07-09 19:13:04 +0200

    Update index.js
    changed some file names

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,20 @@
 import { formHandler } from "./js/formHandler";
-import { postData } from "./js/postData";
 import { updateUi } from "./js/updateUi";
 import { getData } from "./js/getData";
-import { safeData } from "./js/safeData";
+import { geonamesApi } from "./js/callGeonamesApi";
+import { pixabayApi } from "./js/callPixabayApi";
+import { weatherbitApi } from "./js/callWeatherbitApi";
 
 import "./styles/style.scss";
 
 // addEventListener for "click" on button "generate"
 document.getElementById("generate").addEventListener("click", formHandler);
 
-export { formHandler, updateUi, getData, safeData, postData };
+export {
+  formHandler,
+  updateUi,
+  getData,
+  geonamesApi,
+  pixabayApi,
+  weatherbitApi,
+};
