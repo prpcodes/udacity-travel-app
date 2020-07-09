83082be414297a79248f8df039c22c56bbd25bc7
Philipp P <office@prp.wien>
2020-07-09 19:13:46 +0200

    added routes from the server
    added fetches and calls from the server to the client file

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,6 +3,7 @@
 //http://api.geonames.org/searchJSON?q=Vienna&maxRows=10&username=pduoebsi1
 const geoURL = "http://api.geonames.org/searchJSON?q=";
 const apiKey = "&maxRows=10&username=pduoebsi1";
+let geoData = {};
 
 // get form Input
 
@@ -19,22 +20,26 @@ function formHandler() {
   let distance = countDownDate - now;
   // Time calculations for days, hours, minutes and seconds
   let daysUntil = Math.floor(distance / (1000 * 60 * 60 * 24));
+  geoData.daysUntil = daysUntil;
   //get data from API and save it to the server at /data
-  Client.safeData(geoURL, cityName, apiKey)
-    .then((data) => {
-      //save data rout
-      Client.postData("http://localhost:8081/data", {
-        name: data.geonames[0].name,
-        lat: data.geonames[0].lat,
-        lng: data.geonames[0].lng,
-        countryCode: data.geonames[0].countryCode,
-        departure: departure,
-        daysUntil: daysUntil,
+  Client.geonamesApi(geoURL, cityName, apiKey)
+    .then((res) => {
+      geoData.name = res.geonames[0].name;
+      geoData.lat = res.geonames[0].lat;
+      geoData.lng = res.geonames[0].lng;
+      geoData.countryCode = res.geonames[0].countryCode;
+      Client.weatherbitApi(geoData).then((res) => {
+        geoData.currentTemp = res.data[0].temp;
+        geoData.weatherIcon = res.data[0].weather.icon;
+        Client.pixabayApi(cityName).then((res) => {
+          geoData.image = res.hits[0].webformatURL;
+          console.log(geoData);
+          Client.updateUi(geoData);
+        });
       });
     })
-    //then, after all the work on the sever, get the data back for the frontend
-    .then(() => {
-      Client.getData();
+    .catch((err) => {
+      console.log(err);
     });
 }
 
