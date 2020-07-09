8a55238081c5b15101d9855929374bb4ff73c404
Philipp P <office@prp.wien>
2020-07-09 21:32:21 +0200

    deleted getData.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,5 @@
 import { formHandler } from "./js/formHandler";
 import { updateUi } from "./js/updateUi";
-import { getData } from "./js/getData";
 import { geonamesApi } from "./js/callGeonamesApi";
 import { pixabayApi } from "./js/callPixabayApi";
 import { weatherbitApi } from "./js/callWeatherbitApi";
@@ -10,11 +9,4 @@ import "./styles/style.scss";
 // addEventListener for "click" on button "generate"
 document.getElementById("generate").addEventListener("click", formHandler);
 
-export {
-  formHandler,
-  updateUi,
-  getData,
-  geonamesApi,
-  pixabayApi,
-  weatherbitApi,
-};
+export { formHandler, updateUi, geonamesApi, pixabayApi, weatherbitApi };
