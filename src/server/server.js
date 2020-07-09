2cfa29fb1c77b29c1ba82e9fc30d1f348f611652
Philipp P <office@prp.wien>
2020-07-09 19:10:42 +0200

    revamped the server
    the server should just load the files and not call the apis

--- src/server/server.js
+++ src/server/server.js
@@ -24,85 +24,10 @@ app.use(express.static("dist"));
 //log directory to console
 console.log(__dirname);
 
-//get API Key
-const dotenv = require("dotenv");
-dotenv.config();
-
-const weatherbitApiKey = "9724fd8f861241848ed47ffc0ff7998c";
-const pixabayApiKey = "17157704-eccea26d680203f3390b998d1";
-
 // Initialize main route
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
 });
-
-app.post("/data", function (req, res) {
-  // get the data from the frontend and add the incoming data to "geoData = {}"
-  geoData.name = req.body.name;
-  geoData.lat = req.body.lat;
-  geoData.lng = req.body.lng;
-  geoData.countryCode = req.body.countryCode;
-  geoData.departure = req.body.departure;
-  geoData.daysUntil = req.body.daysUntil;
-  // call the weatherbit API and add received data to "geoData = {}"
-  let callWeatherbitApi = new Promise((resolve, reject) => {
-    const lat = geoData.lat;
-    const lng = geoData.lng;
-    weatherbitApi(lat, lng)
-      .then((res) => {
-        resolve(res);
-        geoData.currentTemp = res.data[0].temp;
-        geoData.weatherIcon = res.data[0].weather.icon;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-
-  // call the pixabay API and add received data to "geoData = {}"
-  let callPixabayApi = new Promise((resolve, reject) => {
-    const city = geoData.name;
-    pixabayApi(city)
-      .then((res) => {
-        resolve(res);
-        geoData.image = res.hits[0].webformatURL;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-});
-// send data back to the frontend
-app.get("/getData", (req, res) => {
-  console.log(geoData);
-  res.send(geoData);
-});
-
-// fetch Weatherbit API
-let weatherbitApi = async (lat, lng) => {
-  const weatherbitApiReqURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${weatherbitApiKey}`;
-  const res = await fetch(weatherbitApiReqURL);
-  let result = {};
-  try {
-    result = await res.json();
-  } catch (error) {
-    console.log("error:", error);
-  }
-  return result;
-};
-
-// fetch Pixabay API
-let pixabayApi = async (city) => {
-  const pixabayApiReqURL = `https://pixabay.com/api/?key=${pixabayApiKey}&q=${city}&image_type=photo`;
-  const res = await fetch(pixabayApiReqURL);
-  let result = {};
-  try {
-    result = await res.json();
-  } catch (error) {
-    console.log("error:", error);
-  }
-  return result;
-};
 
 // Setup Server
 const server = app.listen(PORT, () => {
